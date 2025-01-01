247a23109c9de744941292a4db1effb93e8d51ee
EmpressMia <191341940+empressmia@users.noreply.github.com>
2025-01-01 13:24:50 +0100

    updated install script with checking for config folder.

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 
 if [ -d $HOME/Templates ]; then
   if [ ! -f $HOME/Templates/prj.tcl ]; then
-    cp ./prj.tcl $HOME/Templates/prj.tcl
+    cp ./prj.tcl $HOME/Templates/prj.tcl;
   fi
   if [ ! -f $HOME/Templates/vsimCompile.tcl ]; then
-    cp ./vsimCompile.tcl $HOME/vsimCompile.tcl
+    cp ./vsimCompile.tcl $HOME/vsimCompile.tcl;
   fi
 else
   echo "$HOME/Templates does not exist, please create it"
@@ -15,11 +15,16 @@ fi
 
 if [ -d $HOME/bin ]; then
   if [ ! -f $HOME/bin/prjInit ]; then
-    cp prjInit.tcl $HOME/bin/prjInit
+    cp prjInit.tcl $HOME/bin/prjInit;
   fi
   if [ ! -f $HOME/bin/prjGenerate ]; then
-    cp prjGenerate.tcl $HOME/bin/prjGenerate
+    cp prjGenerate.tcl $HOME/bin/prjGenerate;
   fi
 else
   echo "$HOME/bin does not exists, please create it"
+fi
+
+if [ -d $HOME/.config/simpro ]; then
+  ## create config folder for simpro
+  mkdir -p $HOME/.config/simpro;
 fi
